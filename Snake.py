446004ca375bc480988fa139ab53d3261c16b06e
Deadcode87 <128582548+Deadcode87@users.noreply.github.com>
2023-12-18 19:54:31 +0530

    need to create snake body

--- Snake.py
+++ Snake.py
@@ -10,14 +10,29 @@ clock = pygame.time.Clock()
 def rand_vect():
     return pygame.Vector2(random.choice(range(screen.get_width())), random.choice(range(screen.get_height())))
 
+
+snak_pos=[(screen.get_width//2,screen.get_height//2), (screen.get_width//2,screen.get_height//2),(screen.get_width//2,screen.get_height//2)]
+
+def snake(x,y):
+   
+    print(snak_pos)
+    for num_ in range(len(snak_pos)-1):
+        snak_pos[num_+1]=snak_pos[num_]
+    snak_pos[0]=(x,9)
+    for num_ in range(3):
+        pygame.draw.circle(screen, "green", snak_pos[num_], 10)
+    
+
+
 def game():
     running = True
     speed = 5
+    score=str(0)
     x = screen.get_width() // 2
     y = screen.get_height() // 2
     food_pos = rand_vect()
     snake_pos=[]
-
+    font=pygame.font.Font(None, 36)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,12 +50,18 @@ def game():
             y += speed
 
         if pygame.Rect(x, y, 20, 20).colliderect(pygame.Rect(food_pos.x, food_pos.y, 10, 10)):
+            score=str( int(score)+1)
             food_pos = rand_vect()
 
         screen.fill("purple")
-        pygame.draw.circle(screen, "green", (x, y), 10)
-        pygame.draw.circle(screen, "red", (food_pos.x, food_pos.y), 10)
 
+        snake(x,y)
+        pygame.draw.circle(screen, "red", (food_pos.x, food_pos.y), 10)
+        
+        text_surface=font.render(score, True, (255, 255, 255))
+        text_rect=text_surface.get_rect(center=(screen.get_width() // 20, 50))
+        screen.blit(text_surface, text_rect)
+        
         pygame.display.flip()
         clock.tick(60)
 
